c56c82cbd2dc6d5ae3325a4b499825a5565c9a28
Becky Bell <rlbell@alum.bu.edu>
2014-03-19 18:19:42 -0400

    refactored comments_controller.rb create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,16 +11,16 @@ class CommentsController < ApplicationController
 	end
 
 	def create
-		@comment = Comment.create(comment_params)
+		@comment = Comment.new(comment_params) #duplicated code, not DRY-- change Comment.create to Comment.new
 		@comment.submission_id = params[:submission_id]
 		@comment.user_id = current_user.id
-			if @comment.save!
-				flash[:notice] = 'Created a new comment!'
-      	redirect_to submissions_path
-    	else
-      	flash.now[:errors] = @submission.errors.full_messages
-      	render :new
-    	end
+		if @comment.save!
+			flash[:notice] = 'Created a new comment!'
+    	redirect_to submissions_path
+  	else
+    	flash.now[:errors] = @submission.errors.full_messages
+    	render :new
+  	end
 	end
 
 end
